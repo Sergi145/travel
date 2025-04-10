d19a748ef09e69b81e246404320c018f4c10da8e
Sergi145 <chechi14_1986@hotmail.com>
2025-04-10 18:06:34 +0200

    new change v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 
 export default defineConfig({
-    vite: {
-      build: {
-        sourcemap: true,
-        minify: false
-      }
-    }
-  });
+  output: "static",
+});
